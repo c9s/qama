7b3c9ebf88fd528907df7bbcb448223e2f1065c3
c9s <yoanlin93@gmail.com>
2016-07-13 00:46:01 +0800

    Move entry.ts to src/

--- src/entry.ts
+++ src/entry.ts
@@ -1,7 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import QAApp from "./src/QAApp.tsx";
+import QAApp from "./QAApp.tsx";
 console.clear();
-
 var app = React.createElement(QAApp, { "qState": null });
 ReactDOM.render(app, document.getElementById('app'));
