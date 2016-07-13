60d7236da48527107d997561ec34bf6c1595928c
c9s <yoanlin93@gmail.com>
2016-07-13 16:45:05 +0800

    Add qama-react.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var nodeModulesPath = path.resolve(__dirname, 'node_modules');
 var webpack = require('webpack');
 module.exports = {
   'entry': {
-    'bundle': ['./src/entry'],
+    'qama-react': ['./src/entry'],
   },
   'output': {
     'path': __dirname,
