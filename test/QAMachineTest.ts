a2220f55b52116fd499d6cdd7987ed3bddf21083
caasi Huang <caasi.igd@gmail.com>
2016-07-14 12:22:20 +0800

    Add a naive normalization method for #11

--- test/QAMachineTest.ts
+++ test/QAMachineTest.ts
@@ -39,4 +39,45 @@ describe('QAMachine', () => {
     last = m.query(["yes", "no"], init);
     assert.equal("question4", last.question);
   });
+
+  it("normalize JSON", () => {
+		const json = {
+			"question": "Q1",
+			"payload": { "foo": "bar" },
+			"answers": {
+				"A1": {
+					"question": "Q2",
+					"answers": {
+						"A3": null,
+						"A4": {
+							"question": "Q3",
+							"answers": {
+								"A5": null,
+								"A6": null
+							}
+						}
+					}
+				},
+				"A2": {
+					"question": "Q4",
+					"answers": {
+						"A7": null
+					}
+				}
+			}
+		};
+
+    const m = QAMachine.fromJSON(json);
+    assert.equal(m.get(0).question, "Q1");
+    assert.equal(m.get(1).question, "Q2");
+    assert.equal(m.get(2).question, "Q3");
+    assert.equal(m.get(3).question, "Q4");
+    for (let i of [0, 1, 2, 3]) {
+      let s = m.get(i);
+      for (let k in s.answers) {
+        let a = s.answers[k];
+        assert.isOk(typeof a === "number" || a === null);
+			}
+		}
+  });
 });
